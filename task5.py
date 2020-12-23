c5db2ae6479baf81f05253edb4722e50d4ae090e
Viktor <viktor.kramich@yandex.by>
2020-12-23 14:56:24 +0300

    task5

---
+++ task5.py
@@ -0,0 +1,18 @@
+# задание 5
+import math
+from math import sqrt
+
+a = 6
+b = 8
+
+
+c = sqrt(a**2 + b**2)
+
+print(c) # с-длинна гипотенузы
+
+
+s = (a * b) / 2
+print(s) # s- площадь треугольника
+
+
+ku 
