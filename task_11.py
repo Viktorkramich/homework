fdc0b4c5939e4c04b93a08281a53a31096bca4d6
Viktor <viktor.kramich@yandex.by>
2021-01-03 14:45:32 +0300

    11 for gotovo

--- task_11.py
+++ task_11.py
@@ -2,8 +2,6 @@ a = [2, 4, 5, 6, 7, 10, 11]
 
 b = len(a)
 
-print(b)
-
 i = 0
 
 count = 0
@@ -13,3 +11,13 @@ while i < b:
         count += 1
     i += 1
 print(count)
+
+
+ab = [2, 4, 5, 6, 7, 10, 11]
+
+count_1 = 0
+
+for item in ab:
+    if item % 2 == 0:
+        count_1 += 1
+print(count_1)
