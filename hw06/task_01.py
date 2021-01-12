d3230c11a59c596df6a4a1fa37ab42b72f1472d7
Viktor <viktor.kramich@yandex.by>
2021-01-13 00:07:23 +0300

    1 gotovo

---
+++ hw06/task_01.py
@@ -0,0 +1,49 @@
+def inchCm(int):
+    result = int * 2.54
+    return result
+
+# print(inchCm(float(input())))
+
+def cmInch(int):
+    result = int * 0.39
+    return result
+
+def milesKm(int):
+    result = int * 1.60
+    return result
+
+def kmMiles(int):
+    result = int * 0.62
+    return result
+
+def futKg(int):
+    result = int * 2.20
+    return result
+
+def kgFut(int):
+    result = int * 0.45
+    return result
+
+def unGr(int):
+    result = int * 28
+    return result
+
+def grUn(int):
+    result = int * 0.03
+    return result
+
+def galLitr(int):
+    result = int * 3.78
+    return result
+
+def litrGal(int):
+    result = int * 0.26
+    return result
+
+def pinLitr(int):
+    result = int * 0.47
+    return result
+
+def litrPin(int):
+    result = int * 2.11
+    return result
